3ec253f4d9513136ae0d3a8e9bdeeda8bf30bf6b
linraise <linraise@aliyun.com>
2018-02-04 01:45:16 +0800

    use the update-url.json file to store the url of the pac to be updated

---
+++ shadowsocks-csharp/Model/UpdateUrlConfig.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Shadowsocks.Model
+{
+    /*
+     * Data for update 
+     */
+
+    [Serializable]
+    public class UpdateUrlConfig
+    {
+        /// <summary>
+        /// //"https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt";
+        /// </summary>
+        public string GFWLIST_URL = null;
+        /// <summary>
+        /// // "https://raw.githubusercontent.com/breakwa11/breakwa11.github.io/master/ssr/gfwlist.txt";
+        /// </summary>
+        public string GFWLIST_BACKUP_URL = null;
+        /// <summary>
+        /// // "https://raw.githubusercontent.com/breakwa11/breakwa11.github.io/master/ssr/ss_gfw.pac";
+        /// </summary>
+        public string GFWLIST_TEMPLATE_URL = null;
+        /// <summary>
+        /// // "https://raw.githubusercontent.com/breakwa11/breakwa11.github.io/master/update/ssr-win-4.0.xml";
+        /// </summary>
+        public string UpdateNodeURL = null;
+        /// <summary>
+        /// // "https://raw.githubusercontent.com/breakwa11/breakwa11.github.io/master/free/freenodeplain.txt";
+        /// </summary>
+        public string DEFAULT_FEED_URL = null;
+        /// <summary>
+        /// // "https://github.com/breakwa11/shadowsocks-rss/wiki";
+        /// </summary>
+        public string WikiURL = null;
+        /// <summary>
+        /// // "https://github.com/shadowsocksr/shadowsocksr-csharp/issues/new";
+        /// </summary>
+        public string FeedbackURL = null;
+        /// <summary>
+        /// // "https://breakwa11.github.io";
+        /// </summary>
+        public string ProjectURL = null;
+        /// <summary>
+        /// // "https://raw.githubusercontent.com/breakwa11/breakwa11.github.io/master/ssr/ss_lanip.pac";
+        /// </summary>
+        public string PACFromLanIPListURL = null;
+        /// <summary>
+        /// // "https://raw.githubusercontent.com/breakwa11/breakwa11.github.io/master/ssr/ss_white.pac";
+        /// </summary>
+        public string PACFromCNWhiteListURL = null;
+        /// <summary>
+        /// // "https://raw.githubusercontent.com/breakwa11/breakwa11.github.io/master/ssr/ss_white_r.pac";
+        /// </summary>
+        public string PACFromCNOnlyListURL = null;
+        /// <summary>
+        /// // "https://raw.githubusercontent.com/breakwa11/breakwa11.github.io/master/ssr/ss_cnip.pac";
+        /// </summary>
+        public string PACFromCNIPListURL = null;
+
+        public UpdateUrlConfig()
+        {
+            GFWLIST_URL = "https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt";
+            GFWLIST_BACKUP_URL = "https://raw.githubusercontent.com/shadowsocksrr/breakwa11.github.io/master/ssr/gfwlist.txt";
+            GFWLIST_TEMPLATE_URL = "https://raw.githubusercontent.com/shadowsocksrr/breakwa11.github.io/master/ssr/ss_gfw.pac";
+            UpdateNodeURL = "https://raw.githubusercontent.com/shadowsocksrr/breakwa11.github.io/master/update/ssr-win-4.0.xml";
+            DEFAULT_FEED_URL = "https://raw.githubusercontent.com/shadowsocksrr/breakwa11.github.io/master/free/freenodeplain.txt";
+            WikiURL = "https://github.com/shadowsocksrr/shadowsocks-rss/wiki";
+            FeedbackURL = "https://github.com/shadowsocksr/shadowsocksr-csharp/issues/new";
+            ProjectURL = "https://breakwa11.github.io";
+            PACFromLanIPListURL = "https://raw.githubusercontent.com/shadowsocksrr/breakwa11.github.io/master/ssr/ss_lanip.pac";
+            PACFromCNWhiteListURL = "https://raw.githubusercontent.com/shadowsocksrr/breakwa11.github.io/master/ssr/ss_white.pac";
+            PACFromCNOnlyListURL = "https://raw.githubusercontent.com/shadowsocksrr/breakwa11.github.io/master/ssr/ss_white_r.pac";
+            PACFromCNIPListURL = "https://raw.githubusercontent.com/shadowsocksrr/breakwa11.github.io/master/ssr/ss_cnip.pac";
+        }
+    }
+}
---
+++ shadowsocks-csharp/Util/UpdateUrl.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Shadowsocks.Controller;
+using Shadowsocks.Properties;
+using Shadowsocks.Model;
+using System.Text;
+
+namespace Shadowsocks.Util
+{
+    public  class UpdateUrl
+    {
+        private const string _updateUrlConfigFile = "update-url.json";
+
+        private static UpdateUrlConfig _updateConifg = null;
+        public static UpdateUrlConfig URLConfig
+        {
+            get
+            {
+                if (_updateConifg == null)
+                    _updateConifg = new UpdateUrlConfig();
+                return _updateConifg;
+            }
+        }
+        public UpdateUrl()
+        {
+            
+        }
+
+        public void Save()
+        {
+            try
+            {
+                string _path = System.IO.Path.Combine(System.Windows.Forms.Application.StartupPath, _updateUrlConfigFile);
+                using (StreamWriter sw = new StreamWriter(File.Open(_path, FileMode.Create)))
+                {
+                    string jsonString = SimpleJson.SimpleJson.SerializeObject(_updateConifg);
+                    sw.Write(jsonString);
+                    sw.Flush();
+                }
+            }
+            catch (IOException e)
+            {
+                Logging.LogUsefulException(e);
+            }
+        }
+
+        public bool Load()
+        {
+            try
+            {
+                string _fullpath= System.IO.Path.Combine(System.Windows.Forms.Application.StartupPath, _updateUrlConfigFile);
+                if (!File.Exists(_fullpath))
+                {
+                    if (_updateConifg == null)
+                        _updateConifg = new UpdateUrlConfig();
+                    Save();
+                }
+                else
+                {
+                    string configContent = File.ReadAllText(_fullpath);
+                    _updateConifg = SimpleJson.SimpleJson.DeserializeObject<UpdateUrlConfig>(configContent);
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+        private static UpdateUrl _instance = null;
+        public static UpdateUrl Instance()
+        {
+            if (_instance==null)
+            {
+                _instance = new UpdateUrl();
+            }
+            return _instance;
+        }
+    }
+}
